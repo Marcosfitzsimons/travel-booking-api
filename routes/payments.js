197a392110db2e01702ae4b13cbafb63740bdb05
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-07-11 16:30:33 -0300

    Add payment validation

--- routes/payments.js
+++ routes/payments.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import PaymentController from "../controllers/payment.js";
 import PaymentsService from '../services/payments.js'
+import Trip from '../models/Trip.js'
+
 
 import { verifyUser } from "../middleware/verifyToken.js";
 
@@ -9,7 +11,22 @@ const PaymentInstance = new PaymentController(new PaymentsService());
 
 const router = express.Router();
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+
+    const { userId } = req.body;
+    const tripId = req.body.trip._id
+
+    const trip = await Trip.findById(tripId).populate({
+        path: 'passengers',
+        populate: { path: 'createdBy', select: '_id username fullName addressCda addressCapital phone dni image email' },
+        select: 'fullName dni addressCda addressCapital'
+    });
+
+    const existingPassenger = trip.passengers.find(passenger => passenger.createdBy?._id.toString() === userId);
+    if (existingPassenger) {
+        throw new BadRequestError('Ey! Ya tenes boleto para este viaje.')
+    }
+
     PaymentInstance.getPaymentLink(req, res)
 })
 
