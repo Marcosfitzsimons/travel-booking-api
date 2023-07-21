15babe0bf97ac3717d4d495384b9d1afd68a0620
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-07-21 12:54:46 -0300

    Import status codes

--- controllers/sale.js
+++ controllers/sale.js
@@ -1,8 +1,8 @@
+import { StatusCodes } from 'http-status-codes';
 import { NotFoundError } from '../errors/index.js'
 import OverallStat from '../models/OverallStat.js'
 
 export const getSales = async (req, res) => {
-
     const overallStats = await OverallStat.find()
     if (!overallStats) throw new NotFoundError("No se pudo encontrar las estadísticas generales, intentar más tarde.")
 
