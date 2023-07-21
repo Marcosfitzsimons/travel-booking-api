13a484e64c88ac506db714e320ea1fde2ba564d9
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-07-21 12:15:16 -0300

    Add sale controller

---
+++ controllers/sale.js
@@ -0,0 +1,10 @@
+import { NotFoundError } from '../errors/index.js'
+import OverallStat from '../models/OverallStat.js'
+
+export const getSales = async (req, res) => {
+
+    const overallStats = await OverallStat.find()
+    if (!overallStats) throw new NotFoundError("No se pudo encontrar las estadísticas generales, intentar más tarde.")
+
+    res.status(StatusCodes.OK).json(overallStats[0]) // just for this case (2021()
+}
