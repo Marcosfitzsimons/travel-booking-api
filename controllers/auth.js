20e5b19ec0c71f696c9dbf6a8b77c9a557e25ade
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-07-24 10:10:34 -0300

    .

--- controllers/auth.js
+++ controllers/auth.js
@@ -118,7 +118,7 @@ export const sendPasswordLink = async (req, res) => {
             from: process.env.ZOHO_USER,
             to: email,
             subject: "Recuperar contraseña", // change href value
-            text: `Este link es válido por 5 minutos: <a href="http://localhost:5173/forgotpassword/${user._id}/${setUserToken.verifyToken}">Recuperar contraseña</a>`
+            html: `Este link es válido por 5 minutos: <a href="http://localhost:5173/forgotpassword/${user._id}/${setUserToken.verifyToken}">Recuperar contraseña</a>`
         }
 
 
