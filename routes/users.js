648a7d48b2d27f4173a41a8d98c2347a8f0e4f3c
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-08-10 12:11:39 -0300

    Add updateUserStatus route

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,12 @@
 import express from "express";
-import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { deleteUser, getUser, getUsers, updateUser, updateUserStatus } from "../controllers/user.js";
 import { verifyAdmin, verifyUser } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-/*
-router.get('/checkauthentication', verifyToken, (req, res, next) => {
-    res.send('Hello user, you are logged in' )
-})
+// UPDATE USER STATUS
 
-router.get('/checkuser/:id', verifyUser, (req, res, next) => {
-    res.send('Hello user, you are logged in and you can delete your account.')
-})
-
-router.get('/checkadmin/:id', verifyAdmin, (req, res, next) => {
-    res.send('Hello admin, you are logged in and you can delete all accounts.')
-})
-*/
+router.put("/:id/status", verifyAdmin, updateUserStatus)
 
 // UPDATE  
 router.put("/:id", verifyUser, updateUser)
