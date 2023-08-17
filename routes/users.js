2ef7c69f6ff698805df3e3154af2fef4e0190d98
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-08-17 13:04:52 -0300

    Add user addresses route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteUser, getUser, getUsers, updateUser, updateUserStatus } from "../controllers/user.js";
+import { deleteUser, getUser, getUserAddresses, getUsers, updateUser, updateUserStatus } from "../controllers/user.js";
 import { verifyAdmin, verifyUser } from "../middleware/verifyToken.js";
 
 const router = express.Router();
@@ -16,6 +16,9 @@ router.delete("/:id", verifyUser, deleteUser)
 
 // GET
 router.get("/:id", verifyUser, getUser)
+
+// GET
+router.get("/addresses/:id", verifyUser, getUserAddresses)
 
 // GET ALL
 router.get("/", verifyAdmin, getUsers)
