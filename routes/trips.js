20e80c1463afa48f0a697188f6bfa4f75c1a6eb4
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-09-20 12:47:47 -0300

    Add incomes routes

--- routes/trips.js
+++ routes/trips.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTrip, deleteTrip, getTrip, getTrips, updateTrip } from "../controllers/trip.js";
+import { createTrip, deleteTrip, getIncomes, getMonthlyIncomes, getRecentIncomes, getTrip, getTrips, updateTrip } from "../controllers/trip.js";
 import { verifyAdmin, verifyUser } from '../middleware/verifyToken.js'
 
 const router = express.Router();
@@ -15,6 +15,12 @@ router.delete("/:id", verifyAdmin, deleteTrip)
 
 // GET
 router.get("/:userId/:tripId", getTrip)
+
+// GET INCOMES
+// For now public
+router.get("/incomes", verifyAdmin, getIncomes)
+router.get("/recentIncomes", verifyAdmin, getRecentIncomes)
+router.get("/incomes/:year/:month", verifyAdmin, getMonthlyIncomes)
 
 // GET ALL
 router.get("/", getTrips)
