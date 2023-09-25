0c7df91145c8f02e01a9369a3979b30a5cad2769
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-09-25 15:42:17 -0300

    Change monthlyIncome route

--- routes/trips.js
+++ routes/trips.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createTrip, deleteTrip, getIncomes, getMonthlyIncomes, getRecentIncomes, getTrip, getTrips, updateTrip } from "../controllers/trip.js";
+import { createTrip, deleteTrip, getIncomes, getMonthlyIncomes, getTrip, getTrips, updateTrip } from "../controllers/trip.js";
 import { verifyAdmin, verifyUser } from '../middleware/verifyToken.js'
 
 const router = express.Router();
@@ -16,13 +16,14 @@ router.delete("/:id", verifyAdmin, deleteTrip)
 // GET
 router.get("/:userId/:tripId", getTrip)
 
-// GET INCOMES
-// For now public
-router.get("/incomes", verifyAdmin, getIncomes)
-router.get("/recentIncomes", verifyAdmin, getRecentIncomes)
-router.get("/incomes/:year/:month", verifyAdmin, getMonthlyIncomes)
-
 // GET ALL
 router.get("/", getTrips)
+
+// GET INCOMES
+router.get("/incomes", verifyAdmin, getIncomes)
+
+router.get("/monthly-incomes/:year/:month", verifyAdmin, getMonthlyIncomes)
+
+// ADD YEARLY INCOMES
 
 export default router;
