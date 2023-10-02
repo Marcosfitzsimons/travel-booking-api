f59b511f51ab5d22f5dbee8d1a49508152c7ecb7
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-10-02 15:45:49 -0300

    Delete unused files

--- routes/sales.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getSales } from "../controllers/sale.js";
-import { verifyAdmin } from '../middleware/verifyToken.js'
-
-const router = express.Router();
-
-router.get("/", verifyAdmin, getSales)
-
-export default router;
