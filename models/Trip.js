6755f0d292083c972e2cac05b3edc2adc7d0eabb
marcosfitzsimons <marcosfitzsimons@gmail.com>
2024-01-31 10:43:25 -0300

    Change trip validations

--- models/Trip.js
+++ models/Trip.js
@@ -5,7 +5,7 @@ const TripSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Ingresa nombre del viaje.'],
         minLength: [3, 'El nombre del viaje debe tener al menos 3 caracteres.'],
-        maxLength: [40, 'El nombre del viaje no puede tener más de 40 caracteres.']
+        maxLength: [60, 'El nombre del viaje no puede tener más de 60 caracteres.']
     },
     date: {
         type: Date,
@@ -15,13 +15,13 @@ const TripSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Ingresa ubicación de partida del viaje.'],
         minLength: [3, 'Ubicación de partida debe tener al menos 3 caracteres.'],
-        maxLength: [40, 'Ubicación de partida no puede tener más de 40 caracteres.']
+        maxLength: [60, 'Ubicación de partida no puede tener más de 60 caracteres.']
     },
     to: {
         type: String,
         required: [true, 'Ingresa ubicación de llegada del viaje.'],
         minLength: [3, 'Ubicación de llegada debe tener al menos 3 caracteres.'],
-        maxLength: [40, 'Ubicación de llegada no puede tener más de 40 caracteres.']
+        maxLength: [60, 'Ubicación de llegada no puede tener más de 60 caracteres.']
     },
     departureTime: {
         type: String,
@@ -35,13 +35,13 @@ const TripSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'Ingresa precio del viaje.'],
         min: [1, 'El precio mínimo debe ser al menos 1.'],
-        max: [6000, 'El precio máximo no puede ser mayor a 6000.']
+        max: [30000, 'El precio máximo no puede ser mayor a 30000.']
     },
     maxCapacity: {
         type: Number,
         required: [true, 'Ingresa capacidad máxima del viaje.'],
         min: [1, 'La capacidad mínima debe ser al menos 1.'],
-        max: [25, 'La capacidad máxima no puede ser mayor a 25.']
+        max: [30, 'La capacidad máxima no puede ser mayor a 30.']
     },
     available: {
         type: Boolean,
